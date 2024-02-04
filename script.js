e414b99b7f0da95c5e10995de4601951a9e709be
abdul-aual <mdabdulaual38@gmail.com>
2024-02-04 18:52:11 +0600

    all add

---
+++ script.js
@@ -0,0 +1,344 @@
+const questions =
+[
+    {
+        questions: "Hridoy's favourite Athlete ?",
+    answers:
+    [
+        {text: "Shakib Al Hasan", correct:false},
+        {text: "Chris Gyle", correct:false},
+        {text: "Lionel Andrés Messi Cuccitini", correct:true},
+        {text: "Cristiano Ronaldo dos Santos Aveiro", correct:false},
+    ]
+    },
+    {
+        questions: "Which Type of movies does he like ?",
+    answers:
+    [
+        {text: "Romantic", correct:false},
+        {text: "Action Thrilling", correct:false},
+        {text: "Fiction", correct:false},
+        {text: "Crime Thrilling", correct:true},
+    ]
+    },
+    {
+        questions: "Favourite food of him?",
+    answers:
+    [
+        {text: "Rice with beef", correct:true},
+        {text: "Kachhi", correct:false},
+        {text: "Chinese", correct:false},
+        {text: "Water rice with fried fish", correct:false},
+    ]
+    },
+    {
+        questions: "He used to-",
+    answers:
+    [
+        {text: "IOS", correct:false},
+        {text: "Android", correct:true},
+        
+    ]
+    },
+    {
+        questions: "Favourite bike brand?",
+    answers:
+    [
+        {text: "Kawasaki:ninja", correct:false},
+        {text: "Suzuki:gixer", correct:false},
+        {text: "Bajaj:pulsar", correct:true},
+        {text: "Yamaha!", correct:false},
+    ]
+    },
+    {
+        questions: "Favourite Transport?",
+    answers:
+    [
+        {text: "Helicopter", correct:false},
+        {text: "Cycle", correct:false},
+        {text: "Bike", correct:true},
+        {text: "Car", correct:false},
+    ]
+    },
+    {
+        questions: "Sports He likes most-",
+    answers:
+    [
+        {text: "Cricket", correct:false},
+        {text: "Football", correct:true},
+        {text: "Badminton", correct:false},
+        {text: "Bike-Racing", correct:false},
+    ]
+    },
+    {
+        questions: "From below which one is his most Favourite movie?",
+    answers:
+    [
+        {text: "Puspa", correct:true},
+        {text: "KGF-2", correct:false},
+        {text: "Babubali 2", correct:false},
+        {text: "Matha nosto", correct:false},
+    ]
+    },
+    {
+        questions: "He comforted at",
+    answers:
+    [
+        {text: "High-commode", correct:false},
+        {text: "Normal-toilet", correct:true},
+        
+    ]
+    },
+    {
+        questions: "Favourite Actor?",
+    answers:
+    [
+        {text: "SRK", correct:false},
+        {text: "Salman Khan", correct:false},
+        {text: "Allu Arjun", correct:false},
+        {text: "Salman Shah", correct:true},
+    ]
+    },
+    {
+        questions: "Does Hridoy enjoyed somke ever?",
+    answers:
+    [
+        {text: "NO!", correct:true},
+        {text: "YES", correct:false},
+        
+    ]
+    },
+    {
+        questions: "Celebraty Crush?",
+    answers:
+    [
+        {text: "Apee Karim", correct:false},
+        {text: "Nora Fatehi", correct:false},
+        {text: "Shakira", correct:false},
+        {text: "No one", correct:true},
+    ]
+    },
+    {
+        questions: "Company name of the mobile he using currently",
+    answers:
+    [
+        {text: "Symphony", correct:true},
+        {text: "Samsung", correct:false},
+        {text: "Oppo", correct:false},
+        {text: "Realme", correct:false},
+    ]
+    },
+    {
+        questions: "His favourite color?",
+    answers:
+    [
+        {text: "Black", correct:false},
+        {text: "White", correct:false},
+        {text: "Black & White", correct:true},
+        {text: "Green", correct:false},
+    ]
+    },
+    {
+        questions: "He actually wants to be a/an",
+    answers:
+    [
+        {text: "Engineer", correct:false},
+        {text: "Entrepreneur", correct:true},
+        {text: "Govt. Bureaucrat", correct:false},
+        {text: "Teacher", correct:false},
+    ]
+    },
+    {
+        questions: "Social sites he used most",
+    answers:
+    [
+        {text: "Whatsapp", correct:true},
+        {text: "Instagram", correct:false},
+        {text: "Facebook", correct:false},
+        {text: "Telegram", correct:false},
+    ]
+    }
+    ,
+    {
+        questions: "He prefer-",
+    answers:
+    [
+        {text: "Online game", correct:true},
+        {text: "Field game", correct:false},
+        
+    ]
+    },
+    {
+        questions: "He eats-",
+    answers:
+    [
+        {text: "slowly", correct:false},
+        {text: "very slowly", correct:false},
+        {text: "voraciously", correct:false},
+        {text: "fastly", correct:true},
+    ]
+    },
+    {
+        questions: "Favourite Subject-",
+    answers:
+    [
+        {text: "Biology :D", correct:false},
+        {text: "Mathematics", correct:true},
+        {text: "Chemistry", correct:false},
+        {text: "Algorithm", correct:false},
+    ]
+    },
+    {
+        questions: "He loves someone.Do you believe it?",
+    answers:
+    [
+        {text: "Yes!", correct:true},
+        {text: "No!", correct:false},
+        {text: "May-be!", correct:false},
+        {text: "No-emotion", correct:false},
+    ]
+    },
+    {
+        questions: "His mother's name",
+    answers:
+    [
+        {text: "Alayna Rahman", correct:false},
+        {text: "Afroza Begum", correct:false},
+        {text: "Mahmuda Akter", correct:false},
+        {text: "Gulsan Begum", correct:true},
+    ]
+    },
+    {
+        questions: "He enjoys most in -",
+    answers:
+    [
+        {text: "KG", correct:false},
+        {text: "High-School", correct:true},
+        {text: "College", correct:false},
+        {text: "University", correct:false},
+    ]
+    },
+    {
+        questions: "Favourite suits-",
+    answers:
+    [
+        {text: "Panjabi-Payjama", correct:false},
+        {text: "Cort-Tie", correct:false},
+        {text: "3 quartes, T-Shirt", correct:false},
+        {text: "pant-shirt", correct:true},
+    ]
+    },
+    {
+        questions: "He never skips talkshows of ",
+    answers:
+    [
+        {text: "Asif Nazrul Sir", correct:false},
+        {text: "Baristar Rumeen Farhana", correct:true},
+        {text: "Journalist Eliasur Rahman Elias", correct:false},
+        {text: "Golam Mawla Rony", correct:false},
+    ]
+    },
+    {
+        questions: "He Type of devices He liked most to use",
+    answers:
+    [
+        {text: "PC", correct:true},
+        {text: "Laptop", correct:false},
+        {text: "Mobile", correct:false},
+        {text: "Tablet", correct:false},
+    ]
+    }
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+        currentQuestionIndex = 0;
+        score = 0;
+        nextButton.innerHTML = "Next";
+        showQuestion();
+}
+
+function showQuestion()
+{
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    questions;
+
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct)
+        {
+            button.dataset.correct = answer.correct;  
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+
+}
+
+function resetState()
+{
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e)
+{
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>
+    {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+
+function showScore()
+{
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextbutton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length)
+    {
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextbutton();
+    }else{
+        startQuiz();
+    }
+});
+
+startQuiz();
